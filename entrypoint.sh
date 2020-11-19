f0d68c73df02656daf317d5891f84145f2a8cc35
Yang Zheyu <yangzheyu98@gmail.com>
2020-11-19 20:32:44 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@
 echo "$INPUT_SERVICE_KEY" | base64 --decode > "$HOME"/service_key.json
 
 gcloud auth activate-service-account --key-file="$HOME"/service_key.json --project "$INPUT_PROJECT"
-gsutil rm gs://"$INPUT_BUCKET_NAME"/**
-gsutil -m cp -Z "$INPUT_PROJECT" gs://"$INPUT_BUCKET_NAME"
+gsutil rm -r gs://"$INPUT_BUCKET_NAME"/*
+gsutil -m cp -Z -r "$INPUT_PROJECT"/* gs://"$INPUT_BUCKET_NAME"
 #gsutil -m cp -z css,js,map,html "$INPUT_PROJECT" gs://"$INPUT_BUCKET_NAME"
 #gsutil rsync -r -m -d "$INPUT_BUILD_FOLDER" gs://"$INPUT_BUCKET_NAME"
 gsutil web set -m "$INPUT_HOME_PAGE_PATH" -e "$INPUT_ERROR_PAGE_PATH" gs://"$INPUT_BUCKET_NAME"
